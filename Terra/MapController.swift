e0232ed7794afa32ad2785ca67aa0ca57f0b65a0
Cullen Mooney <cullen@protonmail.com>
2018-02-15 22:30:34 -0600

    template marker context created

--- Terra/MapController.swift
+++ Terra/MapController.swift
@@ -10,21 +10,27 @@ import UIKit
 import Mapbox
 
 class MapController: UIViewController, MGLMapViewDelegate {
-
+    
+    @IBOutlet weak var mapViewContainer: UIView!
+    @IBOutlet weak var annotationContext: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let url = URL(string: "mapbox://styles/mapbox/streets-v10")
-        let mapView = MGLMapView(frame: view.bounds, styleURL: url)
+        let mapView = MGLMapView(frame: mapViewContainer.bounds, styleURL: url)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         
         mapView.setCenter(CLLocationCoordinate2D(latitude: 41.8837, longitude: -87.6289), zoomLevel: 9, animated: false)
-        view.addSubview(mapView)
         
         mapView.delegate = self
+        mapViewContainer.addSubview(mapView)
+        
+        annotationContext.isHidden = true
         
         // Allow the map view to display the user's location
         mapView.showsUserLocation = true
+        
         
     }
     
@@ -33,11 +39,13 @@ class MapController: UIViewController, MGLMapViewDelegate {
         // Defining an example marker
         let wrigleyBuilding = MGLPointAnnotation()
         wrigleyBuilding.coordinate = CLLocationCoordinate2D(latitude: 41.8897551, longitude: -87.6247747 )
-        wrigleyBuilding.title = "Wrigley Building"
-        wrigleyBuilding.subtitle = "400 N. Michigan Ave., Chicago, IL 60611"
+        // wrigleyBuilding.title = "Wrigley Building"
+        // wrigleyBuilding.subtitle = "400 N. Michigan Ave., Chicago, IL 60611"
         
         // adding the marker to the map
         mapView.addAnnotation(wrigleyBuilding)
+        
+        annotationContext.isHidden = true
     }
     
     // Use the default marker. See also: our view annotation or custom marker examples.
@@ -45,15 +53,14 @@ class MapController: UIViewController, MGLMapViewDelegate {
         return nil
     }
     
-    // Allow callout view to appear when an annotation is tapped.
-    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
-        return true
-    }
-    
     // Zooming into annotation
     func mapView(_ mapView: MGLMapView, didSelect wrigleyBuilding: MGLAnnotation) {
         let camera = MGLMapCamera(lookingAtCenter: wrigleyBuilding.coordinate, fromDistance: 4000, pitch: 0, heading: 0)
         mapView.setCamera(camera, animated: true)
+    
+        
+        annotationContext.layer.cornerRadius = 5
+        annotationContext.isHidden = false
     }
 
 }
