11b8dbc29afcad5a6dbc6a7da1481633d2223c56
Cullen Mooney <cullen@protonmail.com>
2018-02-25 18:37:54 -0600

    multiple test markers added

---
+++ Terra/BuildingLocations.swift
@@ -0,0 +1,37 @@
+//
+//  BuildingLocations.swift
+//  Terra
+//
+//  Created by Cullen Mooney on 2/25/18.
+//  Copyright © 2018 Cullen Mooney. All rights reserved.
+//
+
+import Foundation
+import Mapbox
+
+class BuildingAnnotation:NSObject,MGLAnnotation{
+    var coordinate: CLLocationCoordinate2D
+    var name: String
+    var address: String
+    var architect: String
+    var year: String
+    var image: String
+    init(_ latitude:CLLocationDegrees,_ longitude:CLLocationDegrees,name:String,address:String,architect:String,year:String,image:String) {
+        self.coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        self.name = name
+        self.address = address
+        self.architect = architect
+        self.year = year
+        self.image = image
+    }
+}
+
+class BuildingAnnotations: NSObject {
+    var buildings: [BuildingAnnotation]
+    
+    override init() {
+        buildings = [BuildingAnnotation(41.889707,-87.624861, name: "Wrigley Building", address: "400-410 N. Michigan Ave., Chicago, IL 60611", architect: "Graham, Anderson, Probst & White", year: "1924", image: "wrigley.png")]
+        buildings += [BuildingAnnotation(41.8866,-87.6249, name: "Carbide & Carbon Building", address: "230 N. Michigan Ave., Chicago, IL 60601", architect: "Burnham Brothers", year: "1929", image: "carbide.png")]
+        buildings += [BuildingAnnotation(41.881826,-87.628151, name: "Chicago Building", address: "7 W. Madison St., Chicago, IL 60602", architect: "Holabird & Roche", year: "1905", image: "chicago.png")]
+    }
+}
--- Terra/MapController.swift
+++ Terra/MapController.swift
@@ -14,11 +14,15 @@ class MapController: UIViewController, MGLMapViewDelegate {
     @IBOutlet weak var mapViewContainer: UIView!
     @IBOutlet weak var annotationContext: UIView!
     
+    var mapView: MGLMapView!
+    
+    let buildingAnnotations = BuildingAnnotations()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let url = URL(string: "mapbox://styles/mapbox/streets-v10")
-        let mapView = MGLMapView(frame: mapViewContainer.bounds, styleURL: url)
+        mapView = MGLMapView(frame: mapViewContainer.bounds, styleURL: url)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         
         mapView.setCenter(CLLocationCoordinate2D(latitude: 41.8837, longitude: -87.6289), zoomLevel: 9, animated: false)
@@ -30,38 +34,38 @@ class MapController: UIViewController, MGLMapViewDelegate {
         // Allow the map view to display the user's location
         mapView.showsUserLocation = true
         
-        setupAnnotationContextView()
-        
-        
+       setupAnnotationContextView()
     }
+
     
     func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
-        
+
         // Defining an example marker
         let wrigleyBuilding = MGLPointAnnotation()
         wrigleyBuilding.coordinate = CLLocationCoordinate2D(latitude: 41.8897551, longitude: -87.6247747 )
-        // wrigleyBuilding.title = "Wrigley Building"
-        // wrigleyBuilding.subtitle = "400 N. Michigan Ave., Chicago, IL 60611"
-        
+
         // adding the marker to the map
-        mapView.addAnnotation(wrigleyBuilding)
+        mapView.addAnnotations(buildingAnnotations.buildings)
+
     }
     
+    
+
     // Use the default marker. See also: our view annotation or custom marker examples.
     func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
         return nil
     }
-    
+
     // Zooming into annotation
     func mapView(_ mapView: MGLMapView, didSelect wrigleyBuilding: MGLAnnotation) {
         let camera = MGLMapCamera(lookingAtCenter: wrigleyBuilding.coordinate, fromDistance: 4000, pitch: 0, heading: 0)
         mapView.setCamera(camera, animated: true)
-    
-        
+
+
         annotationContext.layer.cornerRadius = 5
         annotationContext.alpha = 1.0
     }
-    
+
     func setupAnnotationContextView() {
         annotationContext.alpha = 0
         let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
@@ -71,5 +75,5 @@ class MapController: UIViewController, MGLMapViewDelegate {
         blurView.translatesAutoresizingMaskIntoConstraints = false
         annotationContext.insertSubview(blurView, at: 0)
     }
-
 }
+
